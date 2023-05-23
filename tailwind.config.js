90b2fbb78efdfd5ad6cddbc102e78580123cf3bf
Ali Raza <alirazahamid7@gmail.com>
2023-05-23 23:11:03 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+      },
+      backgroundImage: {
+        "hero-bg": "url('./src/assets/background.jpg')",
+      },
+      fontFamily: {
+        Barlow: "Barlow Condensed",
+        Bellefair: "Bellefair",
+      },
+    },
+  },
+  plugins: [],
+};
+
